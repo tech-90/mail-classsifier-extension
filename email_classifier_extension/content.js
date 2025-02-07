e2f430ce22109d0a8070d899b3bc7bfd2e127107
Dushyant <your-email@example.com>
2025-02-08 02:27:51 +0530

    Initial commit

---
+++ email_classifier_extension/content.js
@@ -0,0 +1,2 @@
+console.log("Email Classifier Content Script Loaded");
+// Further implementation to highlight emails will be added here.
---
+++ email_classifier_extension/popup.js
@@ -0,0 +1,4 @@
+document.getElementById("classify").addEventListener("click", () => {
+  console.log("Classifying Emails...");
+  // Further implementation for fetching emails and running model
+});
